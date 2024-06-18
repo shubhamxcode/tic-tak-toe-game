33a94b6d8f10f58fa847115e161253091a908f64
shubhamxcode <shubh.varshneycode@gmail.com>
2024-06-18 23:04:14 +0530

    winingcondition

--- game.js
+++ game.js
@@ -12,6 +12,29 @@ cells.forEach(cells => {
             return
         } 
         cells.textContent=player;
-        player=player==="X"?"O":"X"
+        if (game(player)) {
+            console.log(`Congratulations! ${player} won the game.`);
+            gameover=true
+        }else{
+            player=player==="X"?"O":"X"
+        }
     })
+    
 });
+
+function game(player){
+    const winningcondition=[
+    [0,1,2],[3,4,5][6,7,8],//for row 
+    [0,3,6],[1,4,7],[2,5,8],//for col
+    [0,4,8],[2,4,6]//for diagonal
+]
+for (const condition of winningcondition) {
+    if (cells[condition[0]].textContent===player &&
+        cells[condition[1]].textContent===player &&
+        cells[condition[2]].textContent===player) {
+        return
+    }
+}
+return false
+   
+};
